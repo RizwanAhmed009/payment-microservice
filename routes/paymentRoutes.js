dcafe77982fea61a00ea4def449f9f76c4593c96
Rizwan Ahmed <Raaj13101@gmail.com>
2025-04-22 16:14:09 +0200

    done changes

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // Create Payment Endpoint
 router.post("/create", async (req, res) => {
   const { user_id, amount, payment_id } = req.body;
-
+// here is the new code suppose
   if (!user_id || !amount || !payment_id) {
     return res
       .status(400)
